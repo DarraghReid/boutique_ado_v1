8b512644b631e4cef893910110b0e120b1331b7e
DarraghReid <darragh.reid@gmail.com>
2021-08-23 14:36:24 +0000

    add product details functionality

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name="products"),
+    # Url for product_detail view in views.py, which contains
+    # product id
+    path('<product_id>', views.product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -14,6 +14,21 @@ def all_products(request):
     # Insert products into context list, which will be passed
     # to the template
     context = {
-        'products': products
+        'products': products,
     }
     return render(request, "products/products.html", context)
+
+
+def product_detail(request, product_id):
+    """
+    View to show individual product details
+    """
+    # Find product in db, by setting Primary Key to product id
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Insert products into context list, which will be passed
+    # to the template
+    context = {
+        'product': product,
+    }
+    return render(request, "products/product_detail.html", context)
